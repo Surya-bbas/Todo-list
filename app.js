a9e3292876368423645adce4cfabec858b3311fe
sasivignesh <sasivignesh51@gmail.com>
2022-10-06 14:17:05 +0530

    heroku version1

--- app.js
+++ app.js
@@ -56,5 +56,5 @@ app.post(`/post`,(req,res)=>{
        
 })
 
-app.listen(3000,() => console.log('the server is running in port 3000'))
+app.listen(process.env.PORT ||3000,() => console.log('the server is running in port 3000'))
 
